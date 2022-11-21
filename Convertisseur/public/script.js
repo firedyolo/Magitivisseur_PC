52fe21c65b06315e5cdad21699587439cc05cf2b
firedyolo <caremejoschwa@gmail.com>
2022-11-21 18:30:45 +0100

    Version - 1.5
    * Possibilité de convertir des playlists Youtube au format mp3
    * Meilleure lisibilité du code
    * Nouvelle interaction entre le serveur et le client
    * Ajout d'un dossier de stockage des musiques enregistrées depuis une Playlist
    * Rajouts de quelques commentaires de code
    * Modification sur le PDF
    * Meilleure lisibilité du texte sur la page (couleurs + claires)

--- Convertisseur/public/script.js
+++ Convertisseur/public/script.js
@@ -1,22 +1,36 @@
 const serverURL = 'http://localhost:3000';
 
+async function checkPlaylist(url) {
+    if (url.indexOf("playlist") === -1) {
+        return "isntlistplay";
+    } else {
+        return "listplay";
+    }
+}
+
 async function checkLink() {
 	if (!document.querySelector('.URL-input').value) {
 		alert("Veuillez indiquer un lien youtube !");
 	} else {
-		await redirect(document.querySelector('.URL-input').value).then(
-			setTimeout(() => {
-				window.location.reload();
-			}, 10000)
-		)
+		let url = document.querySelector('.URL-input').value;
+		let playlist = await checkPlaylist(url);
+		await redirect(url, playlist).then(() => {
+			if (playlist === "listplay") {
+				return;
+			} else {
+				setTimeout(() => {
+					window.location.reload();
+				}, 10000)
+			}
+		})
 	}
 }
 
-async function redirect(url) {
+async function redirect(url, playlist) {
 	const res = await fetch(`${serverURL}/download?url=${url}&fetch=fetch`);
 	if (res.status === 200) {
 		console.log("Oui");
-		window.location.href = `${serverURL}/download?url=${url}&fetch=dwnld`;
+		window.location.href = `${serverURL}/download?url=${url}&fetch=dwnld&islistplay=${playlist}`;
 	} else if (res.status === 400) {
 		alert("Le lien n'est pas bon Kévin !");
 	} else {
--- Convertisseur/server.js
+++ Convertisseur/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+const ytpl = require("ytpl");
+const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
@@ -35,24 +37,31 @@ app.get('/', async function(req, res) {
     });
 });
 
-app.get('/download', async (req, res, next) => {
+app.get('/download', async (req, res) => {
     let url = req.query.url;
     let isFetch = req.query.fetch;
+    let isplaylist = req.query.islistplay;
+    let titleOfPlaylist = [];
 
-    if(!ytdl.validateURL(url)) {
+    if(!ytdl.validateURL(url) && !ytpl.validateID(url)) {
         return res.sendStatus(400);
     }
 
-    if (isFetch === "fetch") return res.sendStatus(200);
+    if (isFetch === "fetch" && isplaylist === undefined) return res.sendStatus(200);
 
+    // Partie pas playlist
     async function getVideoInfos(url) {
         let infos = await ytdl.getBasicInfo(url);
         return {title: infos.videoDetails.title.replace(/[^\w\s]/g, ''), author: infos.videoDetails.author.name}
     }
-    
-    let videoInfos = await getVideoInfos(url);
+
+    async function getTitle(url) {
+        let infos = await ytdl.getBasicInfo(url);
+        return infos.videoDetails.title.replace(/[^\w\s]/g, '');
+    }
 
     async function download(url) {
+        let videoInfos = await getVideoInfos(url);
         console.log(`${getHoraire(new Date())} : Vous avez téléchargé => ${videoInfos.title}`);
         
         res.header('Content-Disposition', `attachment; filename="${videoInfos.title}.mp3"`);
@@ -64,5 +73,33 @@ app.get('/download', async (req, res, next) => {
         filesDownloaded.push({title: videoInfos.title, author: videoInfos.author, horaire: getHoraire(new Date())});
     }
 
-    download(url);
+    async function downloadPlaylist(url, title) {
+        if (fs.existsSync("./playlistSongs")) {
+            ytdl(url, {
+                format: 'mp3',
+                filter: 'audioonly',
+            }).pipe(fs.createWriteStream(`./playlistSongs/${title}.mp3`));
+        } else {
+            return console.log("Veuillez créer un dossier à l'emplacement indiqué comme dans le fichier PDF, nommé : playlistSongs");
+        }
+    }
+
+    async function getPlaylistInfos(url) {
+        let infos = await ytpl(url, {pages: Infinity});
+
+        for (const video of infos.items) {
+            titleOfPlaylist.push(video.id);
+        }
+    }
+
+    //Exécution de la playlist ou pas
+    if (isplaylist === "listplay") {
+        await getPlaylistInfos(url);
+        for (const videoID of titleOfPlaylist) {
+            let title = await getTitle(videoID);
+            downloadPlaylist(videoID, title);
+        }
+    } else {
+        download(url);
+    }
 });
