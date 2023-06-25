033cc5e25f089c56ea80c78597b808e267694c59
firedyolo <caremejoschwa@gmail.com>
2023-06-25 11:21:32 +0200

    Version 1.8.0
    + Refonte visuelle
    + Amélioration de la doc JavaScript
    + Prévient si vous pouvez télécharger la dernière mise-à-jour
    + Ajouts de trois modules : fluent-ffmpeg & homoglyph-search & play-dl
    + Amélioration du système de nommage des fichiers :­ ­
            ­ => Désormais le nom de l'artiste s'ajoute sur le nom du fichier s'il n'apparaît pas (uniquement sur les musiques) (/!\ Peut s'avérer être imprécis dans certains cas /!\)
    + Retrait du poids des fichiers lorsque l'on télécharge au format mp4
    
    BUG FIX :
    - La console affiche désormais le nombre de téléchargements finis dans le bon ordre dans le cas de playlist
    - Le convertisseur ne plante plus lorsqu'une vidéo ne peut pas être téléchargée.
    - Les fichiers téléchargés au format mp3 sont désormais utilisables sur les sites & logiciels (Adobe premiere, Twitch...)

---
+++ Convertisseur/public/request.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ad27eb9a3aa1a88164eb93d2f3b5d4e40edc0993149d729bc650d352063845c
+size 1788
--- Convertisseur/public/script.js
+++ Convertisseur/public/script.js
@@ -1,33 +1,3 @@
-const serverURL = 'http://localhost:3000';
-
-async function checkPlaylist(url) {
-    if (url.indexOf("playlist") === -1) {
-        return "isntplaylist";
-    } else {
-        return "playlist";
-    }
-}
-
-async function checkLink(format) {
-	if (!document.querySelector('.URL-input').value) {
-		alert("Veuillez indiquer un lien youtube !");
-	} else {
-		let url = document.querySelector('.URL-input').value;
-		let playlist = await checkPlaylist(url);
-		await redirect(url, playlist, format);
-		setTimeout(() => {
-			window.location.reload();
-		}, 1500);
-	}
-}
-
-async function redirect(url, playlist, format) {
-	const res = await fetch(`${serverURL}/download?url=${url}&fetch=fetch`);
-	if (res.status === 200) {
-		window.location.href = `${serverURL}/download?url=${url}&fetch=dwnld&isplaylist=${playlist}&format=${format}`;
-	} else if (res.status === 400) {
-		alert("Le lien n'est pas bon Kévin !");
-	} else {
-		alert(`Vous avez possiblement un soucis de connexion. Erreur : ${res.status}`);
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe54e20e6caf7ff16fc3fe2066e6f31e429c15f494b391edc6b65acacef631ba
+size 1225
