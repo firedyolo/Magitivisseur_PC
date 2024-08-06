34793c0cf116903e9b464b7ad7d6387cc65af21b
firedyolo <caremejoschwa@gmail.com>
2024-08-06 22:13:20 +0200

    Version 1.8.2
    + Amélioration de la cohérence visuelle
    + Mise-à-jour du Readme
    + Changement des modules ytdl-core et ytpl par @distube/ytdl-core & @distube/ytpl
    + Ajout d'une alerte lorsque le lien n'est pas valide
    + Retrait du système de prévention des mises à jour car celui-ci ne fonctionne pas
    
    Bug fixes :
    - Le convertisseur ne plante plus
    - Le message concernant les playlists s'affichent correctement

--- Convertisseur/public/script.js
+++ Convertisseur/public/script.js
@@ -1,3 +1,29 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cbc98a9a5764ed0214a0a61ce4c12c1d441977fa249293a2bf39edd5398168e0
-size 1225
+const serverURL = 'http://localhost:3000'; //URL du serveur local
+
+async function checkPlaylist(url) {
+	return url.indexOf("playlist") === -1 ? "isntplaylist" : "playlist";
+}
+
+async function checkLink(format) {
+	//Vérifie si le champ de renseignement du lien est vide ou non
+	if (!document.querySelector('.URL-input').value) {
+		alert("Veuillez indiquer un lien youtube !");
+	} else {
+		let url = document.querySelector('.URL-input').value; //On récupère la valeur du champ de renseignement
+		let playlist = await checkPlaylist(url);
+		
+		await redirect(playlist, format, url); //Permet de renvoyer le lien au serveur pour le téléchargement
+		document.querySelector('.URL-input').value = "";
+	}
+}
+
+async function redirect(playlist, format, url) {
+	const res = await fetch(`${serverURL}/download?url=${url}&fetch=fetch`); // Fais une requête au serveur
+	if (res.status === 200) { // Le serveur répond qu'il n'y a pas d'erreur
+		fetch(`${serverURL}/download?url=${url}&fetch=dwnld&isplaylist=${playlist}&format=${format}`);
+	} else if (res.status === 100) {
+		alert("Le lien n'est pas bon Kévin !");
+	} else {
+		alert(`Vous avez possiblement un soucis de connexion. Erreur : ${res.status}`);
+	}
+}
--- Convertisseur/start.bat
+++ Convertisseur/start.bat
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bc6e225963253919eae3a755b4ea667dc7d9fd520134109a6e0f90f9e331c2d8
-size 14
+node server.js
